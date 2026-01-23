cfca8baf6a2a29c885d4367bb05bd45bd745fb30
Roman Frolov <roman.frolov@purpose-green.com>
2026-01-23 13:28:50 +0100

    fix: add theme support to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,15 +2,18 @@
 
 import { NextIntlClientProvider } from "next-intl";
 
+import { ThemeProvider } from "@/components/providers";
 import { NotFoundTerminal } from "@/components/sections/NotFound";
 import messages from "@/messages/en.json";
 
 export default function RootNotFound() {
   return (
     <NextIntlClientProvider locale="en" messages={messages}>
-      <main className="min-h-[calc(100vh-4rem)] flex items-center justify-center p-6">
-        <NotFoundTerminal />
-      </main>
+      <ThemeProvider>
+        <main className="min-h-[calc(100vh-4rem)] flex items-center justify-center p-6 bg-background">
+          <NotFoundTerminal />
+        </main>
+      </ThemeProvider>
     </NextIntlClientProvider>
   );
 }
